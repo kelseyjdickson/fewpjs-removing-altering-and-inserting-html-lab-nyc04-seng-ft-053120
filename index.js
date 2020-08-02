3151c425a7555b41dd6176c1a62e0a27a6dfae44
kelseyjdickson <kelseymedved@gmail.com>
2020-08-01 21:12:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+// import { isMainThread } from "worker_threads";
+
 // Write your code here!
+var remove = document.getElementById('main');
+remove.remove();
+
+
+var newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "KELSEY is the champion"
